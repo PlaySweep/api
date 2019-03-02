d10a9ac679cf863974bf953ab4ec7f3a84e3ad85
Ryan Waits <ryan.waits@gmail.com>
2019-03-01 22:21:49 -0600

    change REDIS_URL

--- Capfile
+++ Capfile
@@ -36,11 +36,12 @@ install_plugin Capistrano::SCM::Git
 
 require "capistrano/rvm"
 require "capistrano/bundler"
-require "capistrano/rails/migrations"
+# require "capistrano/rails/migrations"
 # require 'capistrano/rails'
 require "capistrano/passenger"
 require 'capistrano/rails/console'
 require 'capistrano-nc/nc'
+require "capistrano-resque"
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,5 @@
 require 'connection_pool'
 
-if Rails.env == 'production'
-  Redis.current = Redis.new(:host => ENV["REDIS_URL"], :port => 6379)
-  Resque.redis = Redis.new(:host => ENV["REDIS_URL"], :port => 6379)
-  Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(:host => ENV["REDIS_URL"], :port => 6379) }
-else
-  Redis.current = Redis.new(:host => 'localhost', :port => 6379)
-  Resque.redis = Redis.new(:host => 'localhost', :port => 6379)
-  Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(:host => 'localhost', :port => 6379) }
-end
+Redis.current = Redis.new(url: ENV["REDIS_URL"])
+Resque.redis = Redis.new(url: ENV["REDIS_URL"])
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(url: ENV["REDIS_URL"]) }
