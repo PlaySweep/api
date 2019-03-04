be49900b8d4d9f8822f5dc6c66dd9a58b6f0d48f
Ryan Waits <ryan.waits@gmail.com>
2019-03-03 23:54:52 -0600

    fix hard coded name issues in messaging

--- app/jobs/send_slate_notification_job.rb
+++ app/jobs/send_slate_notification_job.rb
@@ -7,7 +7,7 @@ class SendSlateNotificationJob < BudweiserJob
       FacebookMessaging::Standard.deliver(user, "Your answers are in, #{user.first_name}! You’ll get your results the morning after the game.", "SILENT_PUSH")
     else
       FacebookMessaging::Standard.deliver(user, "Congratulations on answering all of the questions for your first Budweiser Sweep Contest, #{user.first_name}! You’ll be updated on your status as early as the morning after the game.", "SILENT_PUSH")
-      FacebookMessaging::Standard.deliver(user, "From now on, you can simply ask things and you’ll get a quick response\n\nFor example try invite friends, what are my answers, more games, future contests, and much more.", "SILENT_PUSH")
+      FacebookMessaging::Standard.deliver(user, "From now on, you can simply ask things and you’ll get a quick response\n\nFor example, try typing invite friends, what are my answers, more games, future contests, and much more.", "SILENT_PUSH")
     end
   end
 end
