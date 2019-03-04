5ba0073e66be88c1f76e03336ecb9d2a81b16bf5
Ryan Waits <ryan.waits@gmail.com>
2019-03-04 03:02:18 -0600

    add sponsored param for admin controller

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -2,7 +2,8 @@ class Admin::TeamsController < AdminController
   respond_to :json
 
   def index
-    @teams = Team.ordered.sponsored
+    @teams = Team.ordered.sponsored if params[:sponsored]
+    @teams = Team.ordered
     respond_with @teams
   end
 
