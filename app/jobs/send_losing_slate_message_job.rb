3c8a2aceedfc3ad194ec1ea1f602d836386c4a02
Ryan Waits <ryan.waits@gmail.com>
2019-03-11 00:28:06 -0500

    adjust messaging for NO_PUSH

--- app/jobs/send_losing_slate_message_job.rb
+++ app/jobs/send_losing_slate_message_job.rb
@@ -4,7 +4,7 @@ class SendLosingSlateMessageJob < BudweiserJob
   def perform user_id
     user = BudweiserUser.find(user_id)
     FacebookMessaging::Standard.deliver(user, "#{user.first_name}, unfortunately you didn’t get all 3 right from your Budweiser Sweep card.", "REGULAR")
-    FacebookMessaging::Standard.deliver(user, "On the bright side, there are some more Sweep questions available that you can answer by typing 'More contests' below 👇", "SILENT_PUSH")
+    FacebookMessaging::Standard.deliver(user, "On the bright side, there are some more Sweep questions available that you can answer by typing 'More contests' below 👇", "NO_PUSH")
     # FacebookMessaging::TextButton.deliver(user, "See Available Contests", "On the bright side, there are some more questions available that you can answer by typing 'More contests' below 👇", "SILENT_PUSH")
   end
 end
