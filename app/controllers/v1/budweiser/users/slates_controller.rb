33e0390164565536bc03d9682c08ef9ecac4ee85
Ryan Waits <ryan.waits@gmail.com>
2019-03-15 09:20:51 -0500

    order slates

--- app/controllers/v1/budweiser/users/slates_controller.rb
+++ app/controllers/v1/budweiser/users/slates_controller.rb
@@ -2,7 +2,7 @@ class V1::Budweiser::Users::SlatesController < BudweiserController
   respond_to :json
 
   def index
-    @slates = current_user.slates.available.descending
+    @slates = current_user.slates.descending.available
     respond_with @slates
   end
 
