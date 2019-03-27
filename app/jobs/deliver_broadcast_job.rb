c3b9ecf7c71d2ecdc654a420fce51fb1a9784c6c
Ryan Waits <ryan.waits@gmail.com>
2019-03-27 08:23:13 -0500

    refactor deliver job to map over all teams

--- app/jobs/deliver_broadcast_job.rb
+++ app/jobs/deliver_broadcast_job.rb
@@ -1,16 +1,17 @@
 class DeliverBroadcastJob < ApplicationJob
   @queue = :deliver_broadcast_job
 
-  def perform user_id
-    user = BudweiserUser.find(user_id)
-    conn = Faraday.new(:url => "https://graph.facebook.com/v2.11/me/")
-    params = { message_creative_id: user.preference.team.broadcast_message_id, custom_label_id: user.preference.team.broadcast_label_id}
-    response = conn.post("broadcast_messages?access_token=#{ENV['ACCESS_TOKEN']}", params)
-    broadcast_id = JSON.parse(response.body)["broadcast_id"]
-    if broadcast_id
-      puts "Broadcast Successful 👍"
-    else
-      puts "⁉️" 
+  def perform
+    Team.first(3).each do |team|
+      conn = Faraday.new(:url => "https://graph.facebook.com/v2.11/me/")
+      params = { message_creative_id: team.broadcast_message_id, custom_label_id: team.broadcast_label_id}
+      response = conn.post("broadcast_messages?access_token=#{ENV['ACCESS_TOKEN']}", params)
+      broadcast_id = JSON.parse(response.body)["broadcast_id"]
+      if broadcast_id
+        puts "Broadcast Successful 👍"
+      else
+        puts "⁉️" 
+      end
     end
   end
 end
