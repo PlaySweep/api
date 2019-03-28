e255c82fa9ff406e4e816e7729cf5b416a196dc9
Ryan Waits <ryan.waits@gmail.com>
2019-03-27 23:47:10 -0500

    check if user has preference before subscribing

--- lib/facebook_messaging/broadcast.rb
+++ lib/facebook_messaging/broadcast.rb
@@ -27,14 +27,16 @@ module FacebookMessaging
     def self.subscribe user_id
       begin
         user = User.find_by(id: user_id)
-        conn = Faraday.new(:url => "https://graph.facebook.com/v2.11/#{user.preference.team.broadcast_label_id}/")
-        params = { user: user.facebook_uuid }
-        response = conn.post("label?access_token=#{ENV['ACCESS_TOKEN']}", params)
-        success = JSON.parse(response.body)["success"]
-        if success == true
-          puts "Successfully subscribed user with Broadcast Label 👍"
-        else
-          puts "⁉️"
+        if user.preference.team.try(:broadcast_label_id)
+          conn = Faraday.new(:url => "https://graph.facebook.com/v2.11/#{user.preference.team.broadcast_label_id}/")
+          params = { user: user.facebook_uuid }
+          response = conn.post("label?access_token=#{ENV['ACCESS_TOKEN']}", params)
+          success = JSON.parse(response.body)["success"]
+          if success == true
+            puts "Successfully subscribed user with Broadcast Label 👍"
+          else
+            puts "⁉️"
+          end
         end
       rescue Facebook::Messenger::FacebookError => e
         puts "Facebook Messenger Error message\n\t#{e.inspect} for #{user.facebook_uuid}"
