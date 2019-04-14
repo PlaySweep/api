5181cdcc6de63a29f78500b8ff6981334187a417
Ryan Waits <ryan.waits@gmail.com>
2019-04-13 21:34:52 -0500

    add status to orders

--- app/models/budweiser/order.rb
+++ app/models/budweiser/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  PENDING, AWAITING_DELIVERY, DELIVERED = 0, 1, 2
   belongs_to :user
   belongs_to :prize
+
+  enum status: [ :pending, :awaiting_delivery, :delivered ]
 end
--- db/migrate/20190409232453_create_orders.rb
+++ db/migrate/20190409232453_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
     create_table :orders do |t|
       t.references :user, foreign_key: true, index: true
       t.references :prize, foreign_key: true, index: true
+      t.integer :status, default: 0
       t.timestamps
     end
   end
