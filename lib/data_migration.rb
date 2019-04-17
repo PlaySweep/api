45598395aca6d63300e3b0cf403047e86bd8b52e
Ryan Waits <ryan.waits@gmail.com>
2019-04-17 04:50:15 -0500

    modify preference to roles

--- lib/data_migration.rb
+++ lib/data_migration.rb
@@ -3,7 +3,7 @@ class DataMigration
     Apartment::Tenant.switch!('budweiser')
     Team.active.each do |team|
       User.joins(:preference).where("preferences.data->>'owner_id' = :owner_id", owner_id: team.id.to_s).each do |user|
-        user.add_role team.name.downcase.split(' ').join('_').to_sym, team
+        user.add_role team.name.downcase.split(' ').join('_').to_sym, team unless user.has_role?(team.name.downcase.split(' ').join('_').to_sym, team) 
         print "Switched #{user.first_name} to #{team.name} using Roles."
       end
     end
