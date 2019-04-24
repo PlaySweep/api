9e03013379e6f3a36534f2b8c79ce2b93c5320e0
Ryan Waits <ryan.waits@gmail.com>
2019-04-24 01:49:06 -0500

    fix syntax errors

--- lib/facebook_messaging/standard.rb
+++ lib/facebook_messaging/standard.rb
@@ -16,7 +16,7 @@ module FacebookMessaging
           },
           message_type: "UPDATE",
           notification_type: notification_type
-        }, access_token: credentials.send(Apartment::Tenant.current).try(:access_token)))
+        }, access_token: credentials.send(Apartment::Tenant.current).access_token)
       rescue Facebook::Messenger::FacebookError => e
         puts "Facebook Messenger Error message\n\t#{e.inspect}"
         puts "#{user.full_name} Not found (facebook_uuid: #{user.facebook_uuid})"     
--- lib/facebook_messaging/text_button.rb
+++ lib/facebook_messaging/text_button.rb
@@ -5,7 +5,7 @@ module FacebookMessaging
   class TextButton < Base
     include Facebook::Messenger
 
-    def self.deliver user, title, message, notification_type="REGULAR", url="#{credentials.send(Apartment::Tenant.current).try(:webview_url)}/#{user.facebook_uuid}/dashboard/initial_load"
+    def self.deliver user, title, message, notification_type="REGULAR", url="#{credentials.send(Apartment::Tenant.current).webview_url}/#{user.facebook_uuid}/dashboard/initial_load"
       begin
         Bot.deliver({
           recipient: {
@@ -31,7 +31,7 @@ module FacebookMessaging
           },
           message_type: "UPDATE",
           notification_type: notification_type
-        }, access_token: credentials.send(Apartment::Tenant.current).try(:access_token))
+        }, access_token: credentials.send(Apartment::Tenant.current).access_token)
       rescue Facebook::Messenger::FacebookError => e
         puts "Facebook Messenger Error message\n\t#{e.inspect}"
         puts "#{user.full_name} Not found (facebook_uuid: #{user.facebook_uuid})"     
