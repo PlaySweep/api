21d1cfd300514c231f1b5835b70744793adbfdd2
Ryan Waits <ryan.waits@gmail.com>
2019-05-05 18:47:13 -0500

    adjust start slate to set filtered slate to pending

--- app/jobs/select_winner_job.rb
+++ app/jobs/select_winner_job.rb
@@ -7,21 +7,22 @@ class SelectWinnerJob < BudweiserJob
   end
 
   def find_winner slate
-    winner_collection = slate.cards.win - slate.previous_user_ids
-    loser_collection = slate.cards.loss - slate.previous_user_ids
+    previous_user_ids = slate.previous_user_ids || []
+    winner_collection = slate.cards.win - previous_user_ids
+    loser_collection = slate.cards.loss - previous_user_ids
     found_a_winner = false
 
     until found_a_winner
       if winner_collection.any?
         user_id = winner_collection.sample.user_id
-        slate.previous_user_ids.push(user_id)
-        slate.update_attributes(winner_id: user_id, previous_user_ids: slate.previous_user_ids)
+        previous_user_ids.push(user_id)
+        slate.update_attributes(winner_id: user_id, previous_user_ids: previous_user_ids)
         puts "Found #{User.find(user_id).inspect} in winning cards"
         found_a_winner = true
       else
         user_id = loser_collection.sample.user_id
-        slate.previous_user_ids.push(user_id)
-        slate.update_attributes(winner_id: user_id, previous_user_ids: slate.previous_user_ids)
+        previous_user_ids.push(user_id)
+        slate.update_attributes(winner_id: user_id, previous_user_ids: previous_user_ids)
         puts "Found #{User.find(user_id).inspect} in losing cards"
         found_a_winner = true
       end
--- app/jobs/start_slate_job.rb
+++ app/jobs/start_slate_job.rb
@@ -3,7 +3,7 @@ class StartSlateJob < BudweiserJob
 
   def perform slate_id
     slate = Slate.find_by(id: slate_id)
-    next_slate = slate.class.inactive.ascending.first
+    next_slate = slate.class.filtered(slate.owner_id).inactive.ascending.first
     slate.started! unless slate.nil? || slate.complete? || slate.done?
     next_slate.pending! if next_slate
   end
