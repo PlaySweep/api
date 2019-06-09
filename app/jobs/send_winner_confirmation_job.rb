0e68339bd91a575023119d7c3e622105552bf40c
Ryan Waits <ryan.waits@gmail.com>
2019-06-09 10:34:56 -0500

    add tab before prizing route

--- app/jobs/send_winner_confirmation_job.rb
+++ app/jobs/send_winner_confirmation_job.rb
@@ -5,7 +5,7 @@ class SendWinnerConfirmationJob < BudweiserJob
     slate = Slate.find_by(id: slate_id)
     user = User.find_by(id: user_id)
     FacebookMessaging::Standard.deliver(user, "Congratulations #{user.first_name}, you were selected as our drawing winner 🎉! ", "REGULAR")
-    url = "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/slates/#{slate_id}/confirm_prize"
+    url = "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/slates/#{slate_id}/1/confirm_prize"
     FacebookMessaging::TextButton.deliver(user, "Confirm Now", "Confirm in the next 48 hours to receive your prize 👇", "NO_PUSH", url)
   end
 end
