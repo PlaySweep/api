11c13bb47f8d32549a67e774c6c55a43ab2f3e26
Ryan Waits <ryan.waits@gmail.com>
2019-06-09 01:17:40 -0500

    use redis env connection for prod

--- lib/services/board.rb
+++ lib/services/board.rb
@@ -2,7 +2,7 @@ require 'competition_ranking_leaderboard'
 
 class Board
   def self.fetch leaderboard:
-    CompetitionRankingLeaderboard.new(leaderboard)
+    CompetitionRankingLeaderboard.new(leaderboard, Leaderboard::DEFAULT_OPTIONS, { redis_connection: Redis.new(url: ENV["REDIS_URL"])} )
   end
 
 end
