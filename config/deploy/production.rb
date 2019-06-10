1ea0289bea1114180076c3a34c41e13d21087a83
Ryan Waits <ryan.waits@gmail.com>
2019-06-09 21:38:09 -0500

    add new server to cap deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 # config/deploy/production.rb
 
-server 'bud', user: 'ubuntu', roles: %w{app db web resque_worker resque_scheduler }
+server 'sweep_prod1', user: 'ubuntu', roles: %w{app db web resque_worker resque_scheduler }
+server 'sweep_prod2', user: 'ubuntu', roles: %w{app db web resque_worker resque_scheduler }
 set :deploy_to, "/var/www/sweep_api"
 after "deploy:restart", "resque:restart", "resque:scheduler:restart"
 set :tmp_dir, '/home/deploy/tmp'
@@ -10,8 +11,8 @@ set :rails_env, 'production'
 
 set :linked_files, %w{config/application.yml config/database.yml config/master.key config/locales/en.yml}
 
-role :resque_worker, "bud"
-role :resque_scheduler, "bud"
+role :resque_worker, ["sweep_prod1", "sweep_prod2"]
+role :resque_scheduler, ["sweep_prod1", "sweep_prod2"]
 
 set :resque_environment_task, true
 set :workers, { "*" => 8 }
