4ef613a5598ec2684950cd8a518a6353aa6f7f29
Ryan Waits <ryan.waits@gmail.com>
2019-07-16 20:04:35 -0500

    bud light stuff

--- app/jobs/send_slate_notification_job.rb
+++ app/jobs/send_slate_notification_job.rb
@@ -8,8 +8,8 @@ class SendSlateNotificationJob < BudweiserJob
       FacebookMessaging::Standard.deliver(user, "Nice job #{user.first_name}, your answers are in 👍", "NO_PUSH")
       FacebookMessaging::Carousel.deliver_team(user)
     else
-      FacebookMessaging::Standard.deliver(user, "Congratulations #{user.first_name}, you completed your first Budweiser Sweep Contest!\n\nWe’ll notify you of your results when the games are complete - so feel free to ask things like invite friends, more games, status, and much more in the meantime 👍", "NO_PUSH")
-      FacebookMessaging::Carousel.deliver_team(user)
+      FacebookMessaging::Standard.deliver(user, "Congratulations #{user.first_name}, you completed your first Bud Light Sweep Contest!\n\nWe’ll notify you of your results when the games are complete - and when you start getting thirsty, here's a $10 Drizly credit that you can redeem right now 🍺!", "NO_PUSH")
+      FacebookMessaging::Carousel.deliver_drizly(user)
     end
   end
 end
