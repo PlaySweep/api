a27bdd7552936f9ad4e5a6778f1c6436964e4529
Ryan Waits <ryan.waits@gmail.com>
2019-08-04 14:23:44 -0500

    change admin routes and add global field for products

--- config/routes.rb
+++ config/routes.rb
@@ -38,11 +38,10 @@ Rails.application.routes.draw do
   
   namespace :admin, defaults: { format: :json } do
     resources :leagues, only: [:index]
-    resources :teams, only: [:index, :show] do
-      resources :slates, only: [:index, :show, :create, :update, :destroy] do
-        resources :events, only: [:create, :show, :update, :destroy]
-      end
-      resources :products, only: [:index, :show, :create]
+    resources :teams, only: [:index, :show]
+    resources :slates, only: [:index, :show, :create, :update, :destroy] do
+      resources :events, only: [:create, :show, :update, :destroy]
     end
+    resources :products, only: [:index, :show, :create]
   end
 end
