ec4aa8396a2316d06fb9d4668960abefe7dcc52d
Ryan Waits <ryan.waits@gmail.com>
2019-08-05 07:38:51 -0500

    add conditional for carousel images for team and non-team users

--- lib/facebook_messaging/carousel.rb
+++ lib/facebook_messaging/carousel.rb
@@ -5,53 +5,101 @@ module FacebookMessaging
     include Facebook::Messenger
 
     def self.deliver_team user, url="#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load", quick_replies=nil
-      team = user.roles.find_by(resource_type: "Team").resource
+      team = user.roles.find_by(resource_type: "Team").try(:resource)
       begin
-        @template = {
-          recipient: {
-            id: user.facebook_uuid
-          },
-          message: {
-            attachment: {
-              type: 'template',
-              payload: {
-                template_type: 'generic',
-                elements: [
-          {
-          title: "#{team.abbreviation} Contests",
-          image_url: team.local_image,
-          subtitle: "Make selections for your #{team.name} every day and win awesome prizes!",
-          buttons: [
+        if team
+            @template = {
+              recipient: {
+                id: user.facebook_uuid
+              },
+              message: {
+                attachment: {
+                  type: 'template',
+                  payload: {
+                    template_type: 'generic',
+                    elements: [
+              {
+              title: "#{team.abbreviation} Contests",
+              image_url: team.local_image,
+              subtitle: "Make selections for your #{team.name} every day and win awesome prizes!",
+              buttons: [
+                {
+                  type: :web_url,
+                  url: "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load?tab=1",
+                  title: "Play now",
+                  webview_height_ratio: 'full',
+                  messenger_extensions: true
+                }
+              ]
+            },
             {
-              type: :web_url,
-              url: "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load?tab=1",
-              title: "Play now",
-              webview_height_ratio: 'full',
-              messenger_extensions: true
-            }
-          ]
-        },
-        {
-          title: "Status",
-          image_url: "https://budweiser-sweep-assets.s3.amazonaws.com/fb_status_logo2.png",
-          subtitle: "Check your results or make any changes before the games start!",
-          buttons: [
-            {
-              type: :web_url,
-              url: "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load?tab=3",
               title: "Status",
-              webview_height_ratio: 'full',
-              messenger_extensions: true
+              image_url: "https://budweiser-sweep-assets.s3.amazonaws.com/fb_status_logo2.png",
+              subtitle: "Check your results or make any changes before the games start!",
+              buttons: [
+                {
+                  type: :web_url,
+                  url: "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load?tab=3",
+                  title: "Status",
+                  webview_height_ratio: 'full',
+                  messenger_extensions: true
+                }
+              ]
             }
           ]
-        }
-      ]
+                }
+              }
+            },
+              message_type: "UPDATE",
+              notification_type: "NO_PUSH"
             }
-          }
-        },
-          message_type: "UPDATE",
-          notification_type: "NO_PUSH"
-        }
+        else
+            @template = {
+              recipient: {
+                id: user.facebook_uuid
+              },
+              message: {
+                attachment: {
+                  type: 'template',
+                  payload: {
+                    template_type: 'generic',
+                    elements: [
+                {
+                title: "Budweiser Game of the Day",
+                image_url: "https://budweiser-sweep-assets.s3.amazonaws.com/budweiser_mlb_fb_lockup.png",
+                subtitle: "Make selections for the Budweiser Game of the Day and win awesome prizes!",
+                buttons: [
+                  {
+                    type: :web_url,
+                    url: "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load?tab=1",
+                    title: "Play now",
+                    webview_height_ratio: 'full',
+                    messenger_extensions: true
+                  }
+                ]
+              },
+            {
+              title: "Status",
+              image_url: "https://budweiser-sweep-assets.s3.amazonaws.com/fb_status_logo2.png",
+              subtitle: "Check your results or make any changes before the games start!",
+              buttons: [
+                {
+                  type: :web_url,
+                  url: "#{ENV["WEBVIEW_URL"]}/#{user.facebook_uuid}/dashboard/initial_load?tab=3",
+                  title: "Status",
+                  webview_height_ratio: 'full',
+                  messenger_extensions: true
+                }
+              ]
+            }
+          ]
+                }
+              }
+            },
+              message_type: "UPDATE",
+              notification_type: "NO_PUSH"
+            }
+        end
 
         if quick_replies && quick_replies.any?
           @template[:message][:quick_replies] = quick_replies
