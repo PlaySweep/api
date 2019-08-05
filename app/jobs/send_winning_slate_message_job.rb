9a2d6b05da4811464b5e056313b79407fbeb00f3
Ryan Waits <ryan.waits@gmail.com>
2019-08-04 21:26:46 -0500

    refactor global? logic when closing out

--- app/jobs/send_winning_slate_message_job.rb
+++ app/jobs/send_winning_slate_message_job.rb
@@ -6,8 +6,8 @@ class SendWinningSlateMessageJob < BudweiserJob
     slate = Slate.find(slate_id)
     if slate.global?
       message = "#{slate.name} results inside ⚾️"
-      FacebookMessaging::Standard.deliver(user, message, "REGULAR")
-      context_message = "#{user.first_name}, you got all 5 right and completed a Budweiser Sweep!\n\nYou've been entered into our drawing and will be notified in the next 24 hours if you have been selected as our prize winner!"
+      FacebookMessaging::Standard.deliver(user, "#{message}", "REGULAR")
+      context_message = "#{user.first_name}, you got all 3 right and completed a Budweiser Sweep!\n\nYou've been entered into our drawing and will be notified in the next 24 hours if you have been selected as our #{slate.prizes.first.product.name} winner!"
       FacebookMessaging::Standard.deliver(user, "#{context_message}", "NO_PUSH")
       FacebookMessaging::Carousel.deliver_team(user)
     else
