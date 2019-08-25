d97f1a66718d0c6140ce270421f0db24a8725de6
Ryan Waits <ryan.waits@gmail.com>
2019-08-25 14:38:31 -0500

    fix syntax error

--- app/controllers/v1/users/roles_controller.rb
+++ app/controllers/v1/users/roles_controller.rb
@@ -15,9 +15,9 @@ class V1::Users::RolesController < ApplicationController
       symbolized_role = previous_role.resource.name.downcase.split(' ').join('_').to_sym
       previous_team = previous_role.resource
       user.remove_role(symbolized_role, previous_team)
-      team = Team.find_by(id: params[:team_id]
+      team = Team.find_by(id: params[:team_id])
       team_symbol = params[:team_name].to_sym
-      user.add_role(team_symbol, team))
+      user.add_role(team_symbol, team)
       subscribe_to resource: team, user: user
     end
     FacebookMessaging::Carousel.deliver_team(user)
