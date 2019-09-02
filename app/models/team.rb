ceae0701964c06dd88810c0e4b862617de9b8417
Ryan Waits <ryan.waits@gmail.com>
2019-09-02 13:17:33 -0500

    account for abbreviation having more than one word

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < Owner
   has_many :products, foreign_key: :owner_id
   has_many :slates, foreign_key: :owner_id
 
-  scope :by_name, ->(team_abbreviation) { data_where(abbreviation: team_abbreviation.capitalize) }
+  scope :by_name, ->(team_abbreviation) { data_where(abbreviation: team_abbreviation.split('_').map(&:capitalize).join(' ')) }
   scope :ordered, -> { order(name: :asc) }
   scope :sponsored, -> { data_where(sponsored: true) }
   scope :by_division, ->(division) { data_where(division: division) }
