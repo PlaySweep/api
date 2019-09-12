06f78108c60786c18cfd6dde580886c30195212b
Ryan Waits <ryan.waits@gmail.com>
2019-09-12 11:39:27 -0500

    add option in message for a tie

--- app/jobs/send_winning_slate_message_with_drizly_job.rb
+++ app/jobs/send_winning_slate_message_with_drizly_job.rb
@@ -15,7 +15,7 @@ class SendWinningSlateMessageWithDrizlyJob < ApplicationJob
       FacebookMessaging::Standard.deliver(user, interpolated_drizly_national_winning_slate_copy, "NO_PUSH")
       FacebookMessaging::Carousel.deliver_team(user)
     else
-      message = slate.result == "W" ? "The #{slate.team.abbreviation} won #{slate.score} - view your results inside" : "The #{slate.team.abbreviation} lost #{slate.score} - view your results inside"
+      message = slate.result == "W" ? "The #{slate.team.abbreviation} won #{slate.score} - view your results inside" : slate.result == "L" ? "The #{slate.team.abbreviation} lost #{slate.score} - view your results inside" : "The #{slate.team.abbreviation} tied #{slate.score} - view your results inside"
       FacebookMessaging::Standard.deliver(user, "#{message}", "REGULAR")
       drizly_local_winning_slate_copy = user.account.copies.where(category: "Drizly Local Winning Slate").sample.message
       interpolated_drizly_local_winning_slate_copy = drizly_local_winning_slate_copy % { first_name: user.first_name, event_size: slate.events.size, prize_name: slate.prizes.first.product.name, drizly_value: promotion.value_in_format }
