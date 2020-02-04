96ab13081b5606da58746bfc2c64bd43dd6bcbfb
Ryan Waits <ryan.waits@gmail.com>
2020-02-04 08:40:28 -0600

    comment out schedulers

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -23,11 +23,12 @@ if Rails.env.development?
     Apartment::Tenant.switch(account.tenant) do
       date = DateTime.current.beginning_of_day
       if date.tuesday?
-        leaderboard_name = "week_#{account.current_week - 1}"
+        reward = Reward.find(5)
+        leaderboard_name = "contest_#{reward.name}"
         leaderboard = Board.fetch(leaderboard: leaderboard_name)
         # store leaderboard results into appropriate tables
         leaderboard_history_name = leaderboard_name.split('_').map(&:capitalize).join(' ')
-        history = LeaderboardHistory.new(name: leaderboard_history_name, description: "Most points earned during a weekly period.")
+        history = LeaderboardHistory.new(name: leaderboard_history_name, description: "Most points earned for the Race to the Super Bowl.")
         DataMigration.seed_leaderboard_results(leaderboard: leaderboard_name, history: history)
         # delete the old weeks leaderboard in redis
         # leaderboard.delete_leaderboard
@@ -43,13 +44,13 @@ if Rails.env.production?
   #   @accounts.each { |account| store_and_cleanup_leaderboard(account: account) }
   # end
 
-  scheduler.cron '0 12 * * *' do
-    @accounts.each { |account| fetch_daily_analytics(account: account) }
-  end
+  # scheduler.cron '0 12 * * *' do
+  #   @accounts.each { |account| fetch_daily_analytics(account: account) }
+  # end
 
-  scheduler.cron '0 12 * * 2' do
-    @accounts.each { |account| fetch_weekly_analytics(account: account) }
-  end
+  # scheduler.cron '0 12 * * 2' do
+  #   @accounts.each { |account| fetch_weekly_analytics(account: account) }
+  # end
 
   def fetch_daily_analytics account:
     Apartment::Tenant.switch(account.tenant) do
