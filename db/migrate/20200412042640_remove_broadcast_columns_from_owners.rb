08343e72cb2db183f313cbc2782126724f348408
Ryan Waits <ryan.waits@gmail.com>
2020-04-12 11:40:55 -0500

    remove migration

--- db/migrate/20200412042640_remove_broadcast_columns_from_owners.rb
+++
@@ -1,6 +0,0 @@
-class RemoveBroadcastColumnsFromOwners < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :owners, :broadcast_message_id, :string
-    remove_column :owners, :broadcast_label_id, :string
-  end
-end
