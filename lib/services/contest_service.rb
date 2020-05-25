a31c17bf79d244ad80ebee0a0d9fc7cbff8ec719
Ryan Waits <ryan.waits@gmail.com>
2020-05-25 08:16:54 -0500

    try to check for contest_id? when running rules

--- lib/services/contest_service.rb
+++ lib/services/contest_service.rb
@@ -14,7 +14,7 @@ class ContestService
 
   def playing_reward
     playing_rule = ContestRuleEvaluator.new(@user).playing_rule
-    if reward_active? && playing_rule && @resource.contest_id?
+    if reward_active? && playing_rule && @resource.try(:contest_id?)
       leaderboard = Board.fetch(
         leaderboard: "contest_#{@reward.name}"
       )
@@ -36,7 +36,7 @@ class ContestService
 
   def pick_reward
     pick_rule = ContestRuleEvaluator.new(@user).pick_rule
-    if reward_active? && pick_rule && @resource.contest_id?
+    if reward_active? && pick_rule && @resource.try(:contest_id?)
       leaderboard = Board.fetch(
         leaderboard: "contest_#{@reward.name}"
       )
@@ -47,7 +47,7 @@ class ContestService
 
   def sweep_reward
     sweep_rule = ContestRuleEvaluator.new(@user).sweep_rule
-    if reward_active? && sweep_rule && @resource.contest_id?
+    if reward_active? && sweep_rule && @resource.try(:contest_id?)
       leaderboard = Board.fetch(
         leaderboard: "contest_#{@reward.name}"
       )
