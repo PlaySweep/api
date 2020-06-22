69d6d2b2f60da04c6599349fd81f3cf18812ac9f
Ryan Waits <ryan.waits@gmail.com>
2020-06-22 16:39:03 -0500

    remove shared application.yml

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 # config/deploy/production.rb
 
-server 'budweiser', user: 'ubuntu', roles: %w{ app db web }
+server 'budweiserv2', user: 'ubuntu', roles: %w{ app db web }
 set :deploy_to, "/var/www/sweep_api"
 set :tmp_dir, '/home/deploy/tmp'
 
 set :branch, 'release20200501'
 set :rails_env, 'production'
 
-set :linked_files, %w{config/application.yml config/database.yml config/master.key config/locales/en.yml}
+set :linked_files, %w{config/database.yml config/master.key config/locales/en.yml}
 
 set :pty, false
 set :sidekiq_processes, 2
