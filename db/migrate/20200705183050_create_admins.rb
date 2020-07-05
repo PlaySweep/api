1670f9115326348760cf997b4ab5ca2bfb2fede2
Ryan Waits <ryan.waits@gmail.com>
2020-07-05 13:31:45 -0500

    migrate admins table

--- db/migrate/20200705183050_create_admins.rb
+++ db/migrate/20200705183050_create_admins.rb
@@ -3,8 +3,8 @@ class CreateAdmins < ActiveRecord::Migration[5.2]
     create_table :admins do |t|
       t.string :username
       t.string :password_digest
+
       t.timestamps
     end
-    add_index :admins, :username, unique: true
   end
 end
