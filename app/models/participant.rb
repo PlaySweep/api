bbad82b5b7525b55ec300f6fe36756f03ac5f33a
Ryan Waits <ryan.waits@gmail.com>
2020-07-14 15:48:41 -0500

    add dependent true to participant / player relationship on destroy

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,7 @@
 class Participant < ApplicationRecord
   belongs_to :team, foreign_key: :owner_id
   belongs_to :slate
-  has_one :player
+  has_one :player, dependent: :destroy
 
   scope :ordered, -> { order(field: :asc) }
 end
