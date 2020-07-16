f5c3a35378ba7b6ceca2eb249b35672f10d634db
Ryan Waits <ryan.waits@gmail.com>
2020-07-16 12:33:44 -0500

    update auth controller

--- app/controllers/basic_authentication_controller.rb
+++ app/controllers/basic_authentication_controller.rb
@@ -3,7 +3,8 @@ class BasicAuthenticationController < ActionController::API
   include ActionController::Caching
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-  before_action :http_basic_authenticate
+  # TODO test Basic Auth with Internal.io setup
+  # before_action :http_basic_authenticate
 
   def http_basic_authenticate
     authenticate_or_request_with_http_basic do |username, password|
