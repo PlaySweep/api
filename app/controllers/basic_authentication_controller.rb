4eb6a2b84f840f440b761876e94905328d2bd2e2
Ryan Waits <ryan.waits@gmail.com>
2020-07-16 16:37:30 -0500

    add basic auth to admin routes

--- app/controllers/basic_authentication_controller.rb
+++ app/controllers/basic_authentication_controller.rb
@@ -3,8 +3,7 @@ class BasicAuthenticationController < ActionController::API
   include ActionController::Caching
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-  # TODO test Basic Auth with Internal.io setup
-  # before_action :http_basic_authenticate
+  before_action :http_basic_authenticate
 
   def http_basic_authenticate
     authenticate_or_request_with_http_basic do |username, password|
