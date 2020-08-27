23512d08a90ddb10b1cd2e117106c9a568ebd265
Ryan Waits <ryan.waits@gmail.com>
2020-08-27 10:48:42 -0500

    change verify to GET call

--- config/routes/messaging.rb
+++ config/routes/messaging.rb
@@ -1,7 +1,7 @@
 # api.playsweep.com/v2/messaging/verify?phone_number=4805227771
 
 namespace :messaging, default: { format: :json } do
-  post 'verify', to: 'verification#verify'
+  get 'verify', to: 'verification#verify'
   get 'verify_check', to: 'verification#verify_check'
   get 'verify_user', to: 'verification#verify_user'
 end
