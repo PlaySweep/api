69268d3b98f9f35ed8dd33a15c7b7da073578c39
Ryan Waits <ryan.waits@gmail.com>
2020-08-31 11:42:53 -0500

    update verification code check

--- lib/organizers/handle_verification_check.rb
+++ lib/organizers/handle_verification_check.rb
@@ -18,9 +18,7 @@ class VerifyCodeCheck
         .services("#{ENV["TWILIO_VERIFY_#{context.current_account.app_name.upcase.gsub(' ', '_')}_SERVICE_ID"]}") # TODO find a way to make this dynamic, without having access to user object yet
         .verification_checks
         .create(to: "+#{context.params[:phone_number]}", code: context.params[:code])
-      unless verification_check.status == 'approved'
-        context.fail_and_return!("Failed to verify code.")
-      end
+      verification_check.status == 'approved'
     end
   end
 end
